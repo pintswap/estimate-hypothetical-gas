d3aed32a97ca716d670c0f3450d82221643b225e
pintswap <dev@pintswap.exchange>
2023-06-06 13:19:11 +0000

    update

---
+++ lib/index.d.ts
@@ -0,0 +1,1 @@
+export declare function estimateHypotheticalGas(txParams: any): Promise<number>;
---
+++ src.ts/index.ts
@@ -0,0 +1,3 @@
+export async function estimateHypotheticalGas(txParams: any): Promise<number> {
+  return 0;
+}
